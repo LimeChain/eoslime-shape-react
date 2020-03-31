91ebce968d78ec311b90e556ddacf15f9f78bd9d
vladimirhristov <vlado.og.hristov@gmail.com>
2020-03-31 11:42:19 +0300

    Updated search in contract table

--- tests/todomanager-tests.js
+++ tests/todomanager-tests.js
@@ -3,8 +3,7 @@ const assert = require('assert');
 const TODO_MANAGER_WASM_PATH = './contracts/todomanager.wasm';
 const TODO_MANAGER_ABI_PATH = './contracts/todomanager.abi';
 
-const TODOS_TABLE = 'todos'
-const DESCRIPTION = 'sample description'
+const DESCRIPTION = 'sample description';
 const STATUS_TYPE = { BACKLOG: 0, IN_PROGRESS: 1, REVIEW: 2, DONE: 3 }
 
 describe('TODO Manager', function (eoslime) {
@@ -18,19 +17,19 @@ describe('TODO Manager', function (eoslime) {
     it('Should add a new todo', async () => {
         await managerContract.add(DESCRIPTION);
 
-        const todos = await eoslime.Provider.select(TODOS_TABLE).from(managerContract.name).find()
+        const todos = await managerContract.todos.find();
 
         assert.equal(todos.length, 1, 'Todo is not added');
-        assert.equal(todos[0].description, DESCRIPTION, 'Invalid description')
+        assert.equal(todos[0].description, DESCRIPTION, 'Invalid description');
     });
 
     it('Should update an existing todo', async () => {
         await managerContract.add(DESCRIPTION);
         await managerContract.update(0, STATUS_TYPE.IN_PROGRESS);
 
-        const todos = await eoslime.Provider.select(TODOS_TABLE).from(managerContract.name).find()
+        const todos = await managerContract.todos.find();
 
-        assert.equal(todos[0].status, STATUS_TYPE.IN_PROGRESS, 'Invalid status')
+        assert.equal(todos[0].status, STATUS_TYPE.IN_PROGRESS, 'Invalid status');
     });
 
     it('Should throw updating a non existing todo', async () => {
@@ -40,16 +39,16 @@ describe('TODO Manager', function (eoslime) {
             managerContract.update(1, STATUS_TYPE.IN_PROGRESS)
         );
 
-        const todos = await eoslime.Provider.select(TODOS_TABLE).from(managerContract.name).find()
+        const todos = await managerContract.todos.find();
 
-        assert.equal(todos[0].status, STATUS_TYPE.BACKLOG, 'Invalid status')
+        assert.equal(todos[0].status, STATUS_TYPE.BACKLOG, 'Invalid status');
     });
 
     it('Should remove an existing todo', async () => {
         await managerContract.add(DESCRIPTION);
         await managerContract.remove(0);
 
-        const todos = await eoslime.Provider.select(TODOS_TABLE).from(managerContract.name).find()
+        const todos = await managerContract.todos.find();
 
         assert.equal(todos.length, 0, 'Todo is not removed');
     });
@@ -61,9 +60,9 @@ describe('TODO Manager', function (eoslime) {
             managerContract.remove(1)
         );
 
-        const todos = await eoslime.Provider.select(TODOS_TABLE).from(managerContract.name).find()
+        const todos = await managerContract.todos.find();
 
-        assert.equal(todos.length, 1, 'Todo is removed')
+        assert.equal(todos.length, 1, 'Todo is removed');
     });
 
 });
