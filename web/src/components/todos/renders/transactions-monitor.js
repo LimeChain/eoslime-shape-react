b53e531265ce6811b1ba596172d336bb219efb16
bakasura980 <lyubo@limechain.tech>
2020-04-13 13:44:57 +0300

    Update monitor panel design

--- web/src/components/todos/renders/transactions-monitor.js
+++ web/src/components/todos/renders/transactions-monitor.js
@@ -8,29 +8,23 @@ export const TransactionsMonitorHtml = function (context) {
             const transaction = context.state.txHistory[i];
 
             monitorEntries.push(
-                <div key={transaction.id} className="tx">
-                    <div className="tx-text text-center">{transaction.todoText}</div>
-                    <div className="tx-text w3-flat-turquoise">Action: {transaction.action}</div>
-                    <div className="tx-text">{transaction.id}</div>
+                <div key={transaction.id} className="transaction">
+                    <div className="transaction-text text-center">{transaction.todoText}</div>
+                    <div className="transaction-text">Action: {transaction.action}</div>
+                    <div className="transaction-text">{transaction.id}</div>
                 </div>
             );
         }
     }
 
     return (
-        <div>
-            <div className="monitor-container">
-                <div className="monitor">
-                    <h5 className="monitor-title">Transactions monitor</h5>
-                    <div className="bar">
-                        <div className="emptybar"></div>
-                        <div className="filledbar"></div>
-                    </div>
-                    <div>
-                        {monitorEntries}
-                    </div>
-                </div>
-
+        <div className="monitor">
+            <h5 className="monitor-title">Transactions monitor</h5>
+            <div className="bar">
+                <div className="filledbar"></div>
+            </div>
+            <div className="transactions">
+                {monitorEntries}
             </div>
         </div>
     );
